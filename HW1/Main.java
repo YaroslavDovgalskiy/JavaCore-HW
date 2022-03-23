3af9da8a9e5e4246968a5eda5bca3b0864d0d56b
YaroslavDovgalskiy <99210700+YaroslavDovgalskiy@users.noreply.github.com>
2022-03-23 21:10:51 +0300

    Create Main.java

---
+++ HW1/Main.java
@@ -0,0 +1,16 @@
+package HW1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Course c = new Course(new Cross(80), new Water(3), new Wall(5)); // Создаем полосу препятствий
+        Team team = new Team("PetShop", new Cat("Kitty"), new Dog("Doggy"), new Pig ("Piggy"), new Mouse("Mikky")); // Создаем команду
+        c.doIt(team); // Просим команду пройти полосу
+
+        System.out.println("\nWinners:");
+        team.passedTheDistance();
+
+        System.out.println("\nResult:");// Показываем результаты
+        team.showResults();
+    }
+}
